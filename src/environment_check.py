d3288716b52a178dba5b9855292ff4641ba28ba2
shraddhaa09 <shraddhakhetmalis915@gmail.com>
2026-08-09 16:33:15 +0530

    chore: initialize Python project structure

---
+++ src/environment_check.py
@@ -0,0 +1,6 @@
+import sys
+
+print("AllergyGuard environment check")
+print("-----------------------------")
+print("Python version:", sys.version.split()[0])
+print("Python executable:", sys.executable)
